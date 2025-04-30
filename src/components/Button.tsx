b43533ae1b02853925078b44da2ff7fbba928777
Dipin003 <kharayatdipin@gmail.com>
2025-04-30 21:42:49 +0530

    Implement responsive navbar with enhanced styling

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
+import clsx from "clsx"; 
+
+const classes = cva('border border-white h-12 rounded-full px-6 font-medium', {
+    variants: {
+        variant: {
+            primary: 'bg-lime-400 text-neutral-950 border-lime-400',
+            secondary: 'border-white text-white bg-transparent',
+        }
+    }
+});
+
+type ButtonProps = {
+    variant: "primary" | "secondary";
+} & HTMLAttributes<HTMLButtonElement>;
+
+export default function Button({ variant, className, ...otherProps }: ButtonProps) {
+    return (
+        <button
+            className={clsx(classes({ variant }), className)}
+            {...otherProps}
+        />
+    );
+}
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,3 +1,8 @@
+import Image from "next/image";
+import logoImage from "@/assets/images/logo.svg";
+import Button from "@/components/Button";
+import { a } from "framer-motion/client";
+
 const navLinks = [
     { label: "Home", href: "#" },
     { label: "Features", href: "#features" },
@@ -6,5 +11,42 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <section className="py-4 lg:py-6">
+            <div className="container max-w-5xl">
+                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+                    <div>
+                        <Image src={logoImage} alt="layersLogo" className="h-9 md:h-auto w-auto" />
+                    </div>
+
+                    <div className="lg:flex justify-center items-center hidden">
+                        <nav className="flex gap-6 font-medium">
+                            {navLinks.map((link) => (
+                                <a href={link.href} key={link.label}>
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+
+                    <div className="flex justify-end gap-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                            className="h-6 w-6 text-white mr-2 md:hidden"
+                        >
+                            <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                        </svg>
+
+                        <Button className="hidden md:inline-flex items-center" variant="secondary"> Log In </Button>
+                        <Button
+                        className="hidden md:inline-flex items-center" 
+                        variant="primary"> Sign Up</Button>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
